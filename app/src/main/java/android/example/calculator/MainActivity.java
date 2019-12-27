30f20ba5fae9bb0f5435f0b4fc08e0615eb76c01
TooClose <truproximity@gmail.com>
2019-12-26 19:59:29 -0500

    Been making some progress to a nice first app, feels good.

--- app/src/main/java/android/example/calculator/MainActivity.java
+++ app/src/main/java/android/example/calculator/MainActivity.java
@@ -10,21 +10,40 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int total;
+    private int toBeTotaled;
+
+    public MainActivity() {
+        total = 0;
+        toBeTotaled = 0;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView empty = (TextView) findViewById(R.id.textView);
+        empty.setText("0");
 
+        // One
         Button one = findViewById(R.id.button);
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TextView textView = (TextView) findViewById(R.id.textView);
-                textView.setText(textView.getText() + "1");
+                toBeTotaled += 1;
+                if(toBeTotaled > 1) {
+                    textView.setText(textView.getText()+"1");
+                } else {
+                    textView.setText("1");
+                }
+                System.out.println(toBeTotaled);
+
+
             }
         });
 
+        // Add
         ImageButton add =  findViewById(R.id.add);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
